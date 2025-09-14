261fe6c869f3d0f9f54a5eb7690aeed75c73c53e
Igor Venâncio <igor.mave@gmail.com>
2025-09-14 10:25:30 +0900

    Initial commit, added .gitignore

---
+++ src/main/java/io/github/IgorVenancio/my_kakeibo/MyKakeiboApplication.java
@@ -0,0 +1,13 @@
+package io.github.IgorVenancio.my_kakeibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyKakeiboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyKakeiboApplication.class, args);
+	}
+
+}
